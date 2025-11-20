50451ddf415bdffa0acc166fe1995c4d1fa14610
potatobeast69 <->
2025-11-21 02:30:57 +0700

    111

---
+++ sasas/sasas/sasasApp.swift
@@ -0,0 +1,17 @@
+//
+//  sasasApp.swift
+//  sasas
+//
+//  Created by halfTime on 21/11/25.
+//
+
+import SwiftUI
+
+@main
+struct sasasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
