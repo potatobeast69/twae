b513166c82adde127d719494fff570c75689c762
potatobeast69 <->
2025-11-21 03:12:09 +0700

    112

---
+++ sasas/123.swift
@@ -0,0 +1,18 @@
+//
+//  123.swift
+//  sasas
+//
+//  Created by halfTime on 21/11/25.
+//
+
+import SwiftUI
+
+struct _23: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+
+
+#Preview {
+    _23()
+}
